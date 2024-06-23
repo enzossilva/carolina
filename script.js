af1a440cd3326e5f064d524fca356ccb86b77e71
Enzo Silva <ext_enzarauj@BR00000083WQL43.ml.com>
2024-06-23 11:39:21 -0300

    adjust scroll

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     carousel.addEventListener('scroll', () => {
         const scrollLeft = carousel.scrollLeft;
-        const scrollTop = carousel.scrollTop;
         
-        console.log(`Scroll position - X: ${scrollLeft}, Y: ${scrollTop}`);
+        console.log(`Scroll position - X: ${scrollLeft}`);
+    });
+
+    carousel.addEventListener('wheel', (event) => {
+        // Detecta se o evento de rolagem é de um mouse ou trackpad
+        const isTrackpad = Math.abs(event.deltaY) < 50;
+        
+        if (!isTrackpad) {
+            event.preventDefault(); // Impede a rolagem vertical padrão para mouses
+            carousel.scrollLeft += event.deltaY * 7; // Rola horizontalmente para mouses, multiplicando para maior sensibilidade
+        }
     });
 });
