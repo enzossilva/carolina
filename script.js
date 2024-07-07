809155bd0179dd0cdf7e42bdcda1d2d61057aecc
Enzo Silva <ext_enzarauj@BR00000083WQL43.ml.com>
2024-07-07 16:34:35 -0300

    test cel

--- script.js
+++ script.js
@@ -2,51 +2,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const carousel = document.querySelector('.carousel');
     const images = carousel.querySelectorAll('img');
 
-    // Função para carregar imagens quando elas estão prestes a entrar na visualização
-    const loadImages = () => {
-        images.forEach(img => {
-            if (img.getBoundingClientRect().left < window.innerWidth) {
-                const src = img.getAttribute('data-src');
-                if (src) {
-                    img.src = src;
-                    img.removeAttribute('data-src');
-                }
+    const loadImage = (img) => {
+        const src = img.getAttribute('data-src');
+        if (src) {
+            img.src = src;
+            img.removeAttribute('data-src');
+        }
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                loadImage(entry.target);
+                observer.unobserve(entry.target);
             }
         });
-    };
+    }, {
+        root: carousel,
+        threshold: 0.1
+    });
 
-    // Função de debouncing para a rolagem do carrossel
-    const debounce = (func, wait = 10, immediate = true) => {
-        let timeout;
-        return function() {
-            const context = this, args = arguments;
-            const later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            const callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
-
-    carousel.addEventListener('scroll', debounce(() => {
-        const scrollLeft = carousel.scrollLeft;
-        console.log(`Scroll position - X: ${scrollLeft}`);
-        loadImages();
-    }));
+    images.forEach(img => {
+        observer.observe(img);
+    });
 
     carousel.addEventListener('wheel', (event) => {
-        // Detecta se o evento de rolagem é de um mouse ou trackpad
         const isTrackpad = Math.abs(event.deltaY) < 50;
 
         if (!isTrackpad) {
-            event.preventDefault(); // Impede a rolagem vertical padrão para mouses
-            carousel.scrollLeft += event.deltaY * 7; // Rola horizontalmente para mouses, multiplicando para maior sensibilidade
+            event.preventDefault();
+            carousel.scrollLeft += event.deltaY * 7;
         }
     });
-
-    // Carregar imagens iniciais
-    loadImages();
 });
