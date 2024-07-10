4d331c403bd9ce103e1c5ebe38c1df3f5140e418
Enzo Silva <ext_enzarauj@BR00000083WQL43.ml.com>
2024-07-09 22:22:06 -0300

    adjust size

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const carousel = document.querySelector('.carousel');
-    const images = carousel.querySelectorAll('img');
-
-    carousel.scrollLeft = 0;
-
-    const loadImage = (img) => {
-        const src = img.getAttribute('data-src');
-        if (src) {
-            img.src = src;
-            img.removeAttribute('data-src');
-        }
-    };
-
-    // Carregar a primeira imagem imediatamente
-    loadImage(images[0]);
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                loadImage(entry.target);
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {
-        root: carousel,
-        threshold: 0.1
-    });
-
-    images.forEach(img => {
-        observer.observe(img);
-    });
 
     carousel.addEventListener('wheel', (event) => {
         const isTrackpad = Math.abs(event.deltaY) < 50;
